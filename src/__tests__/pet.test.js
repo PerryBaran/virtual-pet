4db89122a25ff5319038e3b97cbed5f1af16258e
Perry <petreli1@ymail.com>
2022-08-05 11:53:20 +0100

    added pet naming functionality and test

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -3,5 +3,10 @@ const Pet = require('../pet');
 describe('constructor', () => {
     it('returns and object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object)
-    })
+    });
+
+    it('sets name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toBe('Fido')
+    });
 })
