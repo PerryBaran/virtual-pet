8a61abcdda3c1960f34516d35a26d41501901052
Perry <petreli1@ymail.com>
2022-08-05 12:01:49 +0100

    added growUp method and test

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -2,11 +2,23 @@ const Pet = require('../pet');
 
 describe('constructor', () => {
     it('returns and object', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(Object)
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
 
     it('sets name property', () => {
         const pet = new Pet('Fido');
-        expect(pet.name).toBe('Fido')
+        expect(pet.name).toBe('Fido');
     });
+
+    it('sets initial age property to 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toBe(0);
+    });
+
+    it('increases age by 1 when growUp is called', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toBe(0);
+        pet.growUp();
+        expect(pet.age).toBe(1);
+    })
 })
