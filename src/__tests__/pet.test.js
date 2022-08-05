d191aaed8db74798c4691a0c6e2c771b0e9bdc34
Perry <petreli1@ymail.com>
2022-08-05 12:06:30 +0100

    added hunger and fitness properties, updated growUp method and added unit tests

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -20,5 +20,29 @@ describe('constructor', () => {
         expect(pet.age).toBe(0);
         pet.growUp();
         expect(pet.age).toBe(1);
-    })
+    });
+
+    it('sets initial hunger propety to 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toBe(0);
+    });
+
+    it('increases hunger by 5 when growUp is called', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toBe(0);
+        pet.growUp();
+        expect(pet.hunger).toBe(5);
+    });
+
+    it('sets initial fitness propety to 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toBe(10);
+    });
+
+    it('decreases fitness by 3 when growUp is called', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toBe(10);
+        pet.growUp();
+        expect(pet.fitness).toBe(7);
+    });
 })
